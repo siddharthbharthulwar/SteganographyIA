cb7ab665ef93e3bc8721a6d66fa72ff193250d7e
Siddharth Bharthulwar <47193327+sbharthulwar01@users.noreply.github.com>
2020-03-01 18:15:58 -0700

    small fixes

--- encryptionfunctions.py
+++ encryptionfunctions.py
@@ -20,12 +20,27 @@ def map(text):
         intList.append(charList.index(text[i]) + 1)
     return intList
 
+def chooseMax(img, text):
+    width = img.shape[1]
+    height = img.shape[0]
+    size = width * height
+    mapped = map(text)
+    sumZ = 0
+    for i in range(len(mapped)):
+        sumZ += mapped[i]
+  #  return sumZ
+    return int(size / sumZ)
+
 def encrypt(img, text, max):
+    param = img
     width = img.shape[1]
     height = img.shape[0]
     mapped = map(text)
+    print(mapped)
     index = 0
     column = 0
+        
+
     for i in range(len(mapped)):
         val = mapped[i]
 
@@ -33,34 +48,56 @@ def encrypt(img, text, max):
 
         for j in range(div):
             if (index < height):
-                print(index, column)
-                img[index, column] +=1
+                #print(index, column)
+                param[index, column] +=1
                 index +=1
             else:
                 index = 0
                 column +=1
-                img[index, column] +=1
+                param[index, column] +=1
                 index +=1
 
         if (index < height):
-            img[index, column] += val - (truncate(val / max) * max)
+            param[index, column] += val - (truncate(val / max) * max)
             index +=1
         else:
             index = 0
             column +=1
-            img[index, column] += val - (truncate(val / max) * max)
+            param[index, column] += val - (truncate(val / max) * max)
             index +=1
         
         if (index < height):
-            img[index, column] = img[index, column]
+            param[index, column] = param[index, column]
             index +=1
         else:
             index = 0
             column +=1
-            img[index, column] = img[index, column]
+            param[index, column] = param[index, column]
             index +=1
-    return img
+    return param
+
+def decrypt(img, encryptedimg):
+    max = 1 #TODO: automatically encrypt and detect @MAX
+    index = 0
+    column = 0
+    sub = np.abs(np.subtract(img, encryptedimg))
+    return sub
 
 img = cv.imread('black.png', cv.IMREAD_GRAYSCALE)
-plt.imshow(encrypt(img, "hello world", 2))
+im2 = cv.imread('black.png', cv.IMREAD_GRAYSCALE)
+file = open('data.txt',mode='r')
+my = file.read()
+
+plt.imshow(img)
+plt.show()
+
+max = chooseMax(img, my)
+encrypted = (encrypt(img, my, 1))
+plt.imshow(encrypted)
+plt.show()
+
+plt.imshow(im2)
+plt.show()
+
+plt.imshow(decrypt(im2, encrypted))
 plt.show()
